8c7bc6ef349229cc305212f335dc4f3230792f27
SumanMohanty <suman.mohanty043@gmail.com>
2026-05-14 13:13:50 +0530

    Intial Project Setup

---
+++ src/main/java/com/example/login_Management_App/LoginManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.login_Management_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/login_Management_App/LoginManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.login_Management_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
